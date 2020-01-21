f8f84098182c57a3df740435398d16f321479ae0
Michal Barer <michalb1298@gmail.com>
2020-01-21 14:41:29 +0200

    Changed screenshot naming

--- capture.py
+++ capture.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from datetime import datetime
 from urllib.parse import urlparse
 from selenium import webdriver
 
@@ -14,6 +15,11 @@ def start_selenium_driver(url):
     return driver
 
 
+def format_screenshot_name(url):
+    website_name = urlparse(url).netloc.split('.')[0]
+    return website_name + datetime.now().strftime('%m%d%y%H%M%S')
+
+
 def main():
     if len(sys.argv) == 1:
         print("usage: capture.py <url>")
@@ -21,10 +27,12 @@ def main():
     else:
         url = sys.argv[1]
         driver = start_selenium_driver(url)
+
         if not os.path.isdir("screenshots"):
             os.makedirs("screenshots")
+
         driver.find_element_by_tag_name('body')\
-            .screenshot('screenshots/{}.png'.format(urlparse(url).netloc))
+            .screenshot('screenshots/{}.png'.format(format_screenshot_name(url)))
 
 
 if __name__ == '__main__':
